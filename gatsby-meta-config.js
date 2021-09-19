a1f5caa42be2552104e748fa37d8c3f1d5037f43
YunTroubleshooter <cairofaith@gmail.com>
2021-09-20 04:36:34 +0900

    feat: 구글 애널리틱스 id 변경

--- gatsby-meta-config.js
+++ gatsby-meta-config.js
@@ -9,7 +9,7 @@ module.exports = {
       repo: `yjksw/yjksw.github.io`, // `zoomkoding/zoomkoding-gatsby-blog`,
     },
   },
-  ga: 'G-Y0VVV499R1', // Google Analytics Tracking ID
+  ga: 'G-FD2W3E8ERN', // Google Analytics Tracking ID
   author: {
     name: `김윤정`,
     bio: {
